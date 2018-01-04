84424f567cd9442b7012658369b09ddfe65914ef
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-04 20:06:26 +0530

    Create print no of pairs in array.c

---
+++ C/print no of pairs in array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,a[10];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            printf("(%d,%d)",a[i],a[j]);
+        }
+    }
+    return 0;
+}
