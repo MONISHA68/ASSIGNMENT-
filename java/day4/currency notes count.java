76e7f9dbb549edbc8d997c4c7accab4b7fa9dd9a
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-06-29 19:58:42 +0530

    Update currency notes count.java

--- java/day4/currency notes count.java
+++ java/day4/currency notes count.java
@@ -4,19 +4,43 @@ import java.util.Scanner;
 
 public class Currencycount 
 {
-	static int currency(int[] amt,int n)
+	static int currency(int n)
 	{
-		int count=0,rem=0;
+		int count=0;
 		while(n!=0)
 		{
-			rem=n%10;
-			for(int i=0;i<amt.length;i++)
+			if(n>=1000) 
 			{
-				if(rem>=amt[i])
-				{
-					rem*amt[i];
-				}
+				n=n-1000;
+				count++;
+				continue;
 			}
+			if(n>=500) 
+			{
+				n=n-500;
+				count++;
+			}
+			if(n>=100) 
+			{
+				n=n-100;
+				count++;
+			}
+			if(n>=50) 
+			{
+				n=n-50;
+				count++;
+			}
+			if(n>=10) 
+			{
+				n=n-10;
+				count++;
+			}
+			if(n>=1) 
+			{
+				n=n-1;
+				count++;
+			}
+				
 		}
 		return count;
 	}
@@ -24,8 +48,7 @@ public class Currencycount
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int[] amt= {1000,500,100,50,1};
-		int res=currency(amt,n);	
+		int res=currency(n);	
 		System.out.print(res);
 		sc.close();
 	}
