8b5f146ad2dbc69b9a2404cbdd53e0d680a2dba6
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-06-30 16:04:12 +0530

    Create even num odd sum and odd num even sum in array.java

---
+++ java/day4/even num odd sum and odd num even sum in array.java
@@ -0,0 +1,47 @@
+package day4.java;
+
+import java.util.Scanner;
+
+public class Oddevensum 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int sum=0,n=0;
+		int[] a=new int[n1];
+		for(int i=0;i<n1;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n1;i++)
+		{
+			sum=0;
+			n=a[i];
+			if(n%2!=0)
+			{
+				sum=sum+n;
+				while(sum%2!=0)
+				{
+					i++;
+					sum=sum+a[i];
+				}
+				System.out.print(sum+" ");
+				sum=0;
+			}
+			else
+			{
+				sum=0;
+				sum=sum+n;
+				while(sum%2==0)
+				{
+					i++;
+					sum=sum+a[i];
+				}
+				System.out.print(sum+" ");
+				sum=0;
+			}
+		}
+		sc.close();
+	}
+}
