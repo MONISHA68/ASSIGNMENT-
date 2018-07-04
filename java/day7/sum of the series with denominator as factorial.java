fa77b0456ba60e90ad40608b839f36a324eedcec
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-07-04 15:44:09 +0530

    Create sum of the series with denominator as factorial.java

---
+++ java/day7/sum of the series with denominator as factorial.java
@@ -0,0 +1,33 @@
+package day7.java;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Sumoftheseries 
+{
+	public static double series(int n)
+	{
+		double num=Math.pow(n,n);
+		int fact=1;
+		while(n!=0)
+		{
+			fact=fact*n;
+			n--;
+		}
+		double res=num/(double)fact;
+		return res;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		double sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+series(i);
+		}
+		System.out.print(sum);
+		sc.close();
+	}
+
+}
