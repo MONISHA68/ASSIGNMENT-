300150f428da19cff8b5af3ef0b648bc295bbfa5
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-07-08 11:32:25 +0530

    Update player sequence.java

--- java/day8/player sequence.java
+++ java/day8/player sequence.java
@@ -1,66 +1,72 @@
-package day8.java;
+package day7.java;
 
 import java.util.Scanner;
-public class Playersequence
+
+public class Playersincreasingsequence 
 {
-	public static void main(String[] args) 
-	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		String[] str=new String[n];
-		int[] a=new int[n];
-		int t=0;
-		String temp="";
-		for(int i=0;i<n;i++)
+		public static void main(String[] args) 
 		{
-			str[i]=sc.next();
-		}
-		for(int i=0;i<n;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		for(int i=0;i<n-1;i++)
-		{
-			for(int j=i+1;j<n;j++)
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			String[] str=new String[n];
+			int[] a=new int[n];
+			int t=0;
+			String temp="";
+			for(int i=0;i<n;i++)
 			{
-				if(a[i]>a[j])
+				str[i]=sc.next();
+			}
+			for(int i=0;i<n;i++)
+			{
+				a[i]=sc.nextInt();
+			}
+			for(int i=0;i<n-1;i++)
+			{
+				for(int j=i+1;j<n;j++)
 				{
-					t=a[i];
-					temp=str[i];
-					a[i]=a[j];
-					str[i]=str[j];
-					a[j]=t;
-					str[j]=temp;
+					if(a[i]>a[j])
+					{
+						t=a[i];
+						temp=str[i];
+						a[i]=a[j];
+						str[i]=str[j];
+						a[j]=t;
+						str[j]=temp;
+					}
 				}
 			}
-		}
-		int count=1;
-		for(int i=0,j=i+1;i<n && j<n;i++,j++)
-		{
-			if(a[i]+1==a[j])
+			int count=1,x=0;
+			for(int i=0,j=i+1;i<n && j<n;i++,j++)
 			{
-				count++;
+				if(a[i]+1==a[j])
+				{
+					if(count==1)
+					{
+						x=i;
+					}
+					count++;
+				}
+				else
+				{
+					count=1;
+					continue;
+				}
+				if(count==3)
+				{
+					break;
+				}
+			} 
+			if(count==3 )
+			{
+				for(int i=x;i<=x+2;i++)
+				{
+					System.out.println(str[i]+" : "+a[i]);
+				}  
 			}
 			else
 			{
-				break;
+				System.out.print("there is no sequence");
 			}
-			if(count==3)
-			{
-				break;
-			}
-		} 
-		if(count==3)
-		{
-			for(int i=0;i<=2;i++)
-			{
-				System.out.println(str[i]+" : "+a[i]);
-			}  
+			sc.close();
 		}
-		else
-		{
-			System.out.print("there is no sequence");
-		}
-		sc.close();
-	}
 }
